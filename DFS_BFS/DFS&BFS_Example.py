3915c5e116651b0ee3a26b3d31940c5411bdba98
Kangjunseo <mind2749@naver.com>
2021-06-03 17:10:59 +0900

    search

--- DFS_BFS/DFS&BFS_Example.py
+++ DFS_BFS/DFS&BFS_Example.py
@@ -1,72 +1,61 @@
-"""
-1. 특정한 지점의 주변 상, 하, 좌, 우를 살펴본 뒤에 주변 지점 중에서 값이 0이면서 아직 방문하지 않은 지점을 방문
-2. 방문한 지점에서 다시 상하좌우를 살펴보면서 방문을 진행하는 과정을 반복하면 연결된 모든 지점을 방문
-3. 모든 노드에 대해서 1~2번의 과정을 반복, 방문하지 않은 지점의 수를 카운트
-"""
+def dfs(graph, v, visited):
+    #현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-n, m = map(int, input().split())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
 
-# x, y = 0, 0
+visited = [False] * 9
 
-def dfs(x, y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    else:
-        #현재 노드를 아직 방문하지 않았다면
-        if graph[x][y ] == 0:
-            # 해당 노드 방문 처리
-            graph[x][y] = 1
-            # 상하좌우 위치들 모두 재귀적으로 호출
-            dfs(x - 1, y)
-            dfs(x + 1, y)
-            dfs(x, y - 1)
-            dfs(x, y + 1)
-            return True
-        return False
-
-result = 0
-
-for i in range(n):
-    for j in range(m):
-        if dfs(i, j) == True:
-            result += 1
-
-print(result)
-
-###example 4 미로탈출
-n, m = map(int, input().split())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+dfs(graph, 1, visited)
 
 
-dx = [1, -1, 0, 0] # 동 서 남 북
-dy = [0, 0, 1, -1]
 
 from collections import deque
+def bfs(graph, start, visited):
+    queue = deque([start])
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
+    #현재 노드를 방문 처리
+    visited[start] = True
+
+    #큐가 빌때까지 반복
     while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        
+        #해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
 
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
+visited = [False] * 9
 
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-
-    return graph[n-1][m-1]
-
-print(bfs(0, 0))
+bfs(graph, 1, visited)
