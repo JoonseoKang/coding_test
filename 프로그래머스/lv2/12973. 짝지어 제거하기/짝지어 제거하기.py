12d6c94188a95986695a57d1012af065459edfa3
Jaykang <33209250+JoonseoKang@users.noreply.github.com>
2023-02-10 10:04:41 +0900

    [level 2] Title: 짝지어 제거하기, Time: 124.52 ms, Memory: 14.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(s):
+    answer = 1
+    stack = []
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        elif stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
